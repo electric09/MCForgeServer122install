5632b6f69421793fbec2ccb937c10589335f9974
electric09 <102570278+electric09@users.noreply.github.com>
2022-03-29 07:40:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 apt-get update  # To get the latest package lists
 apt-get install ansible -y
-sudo ansible-playbook mcinst.yml
+sudo ansible-playbook mcsinst.yml
 #etc.
