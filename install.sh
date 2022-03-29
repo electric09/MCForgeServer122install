61462a3c99c4149c0dec80c4dcc06ae9a18cf0d5
electric09 <102570278+electric09@users.noreply.github.com>
2022-03-29 06:41:14 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+apt-get update  # To get the latest package lists
+apt-get install ansible -y
+#etc.
